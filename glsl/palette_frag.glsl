cdaeb150b067b11b7ce75328a828afb932fdc392
Johan Forsberg <johan@slentrian.org>
2020-03-28 01:17:20 +0100

    More efficient view rendering.

--- glsl/palette_frag.glsl
+++ glsl/palette_frag.glsl
@@ -2,11 +2,13 @@
 
 precision lowp float;
 
-layout (binding = 0) uniform usampler2D image;
+layout (binding = 0) uniform usampler2DArray image;
 layout (binding = 1) uniform usampler2D overlay;
 
 layout (location = 1) uniform float global_alpha = 1;
-layout (location = 2) uniform vec4[256] palette;
+layout (location = 2) uniform ivec2 slice;
+layout (location = 3) uniform uint transparent_color = 0;
+layout (location = 4) uniform vec4[256] palette;
 
 in VS_OUT {
   vec2 texcoord;
@@ -14,17 +16,28 @@ in VS_OUT {
 
 layout (location=0) out vec4 color;
 
+
 void main(void) {
-  uvec4 pixel = texture(image, fs_in.texcoord);
   uvec4 over_pixel = texture(overlay, fs_in.texcoord);
   uint index;
   if (over_pixel.a > 0) {
     index = over_pixel.r;
     vec4 color_ = palette[index];
-    if (color_.a == 0)
+    if (color_.a == 0) {
       discard;
+    }
     color = vec4(color_.rgb, global_alpha);
   } else {
+    uvec4 pixel;
+    int depth = slice[1] - slice[0];
+    for (int i = 0; i < depth; i++) {
+      // TODO Could not get the loop to work counting down; am I stupid or is this a limitation?
+      pixel = texture(image, vec3(fs_in.texcoord, slice[1] - 1 - i));
+      if (pixel.r != transparent_color) {
+        // Take the topmost non transparent pixel.
+        break;
+      }
+    } 
     index = pixel.r;
     vec4 color_ = palette[index];
     if (pixel.a == 0 || color_.a == 0)
--- voxpaint/render.py
+++ voxpaint/render.py
@@ -5,10 +5,10 @@ from pyglet import gl
 
 from fogl.framebuffer import FrameBuffer
 from fogl.shader import Program, VertexShader, FragmentShader
-from fogl.texture import Texture, ByteTexture, ImageTexture
+from fogl.texture import Texture, ByteTexture3D
 from fogl.vao import VertexArrayObject
 
-from .texture import IntegerTexture, ByteIntegerTexture
+from .texture import IntegerTexture
 
 
 EMPTY_COLOR = (gl.GLfloat * 4)(0, 0, 0, 0)
@@ -16,76 +16,90 @@ EMPTY_COLOR = (gl.GLfloat * 4)(0, 0, 0, 0)
 
 def render_view(window):
 
+    """ Render the current view to a texture. """
+
     drawing = window.drawing
     view = window.view
     data = view.data
     w, h, d = view.shape
     size = w, h
-    ob = _get_offscreen_buffer(size)
+    offscreen_buffer = _get_offscreen_buffer(size)
     colors = _get_colors(drawing.palette.colors)
     
-    gl.glClearBufferfv(gl.GL_COLOR, 0, (gl.GLfloat * 4)(0.25, 0.25, 0.25, 1))
+    # Update the overlay with the current stroke
+    overlay = view.overlay
+    overlay_texture = _get_overlay_texture(size)
+    if overlay.dirty and overlay.lock.acquire(timeout=0.01):
+        rect = overlay.dirty
+        x0, y0, x1, y1 = rect.box()
+        overlay_data = overlay.data[x0:x1, y0:y1].tobytes("F")
+        gl.glTextureSubImage2D(overlay_texture.name, 0, *rect.position, *rect.size,
+                               gl.GL_RGBA_INTEGER, gl.GL_UNSIGNED_BYTE, overlay_data)
+        overlay.dirty = None
+        overlay.lock.release()
+
+    # Update the image texture
+    gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)  # Needed for writing 8bit data
+    drawing_texture = _get_3d_texture(view.shape)        
+    for i in range(d):
+
+        if not view.layer_visible(i):
+            continue
+
+        dirty = view.dirty[i]
+        if dirty and drawing.lock.acquire(timeout=0.01):
+            print("hej")
+            layer = data[:, :, i]
+            layer_data = layer.tobytes("F")  # TODO maybe there's a better way?
+            gl.glTextureSubImage3D(drawing_texture.name, 0,
+                                   0, 0, i, w, h, 1,  # TODO use dirty rect
+                                   gl.GL_RED_INTEGER, gl.GL_UNSIGNED_BYTE,
+                                   layer_data)
+            view.dirty[i] = None
+    gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 4)
+
+    # Render everything to the offscreen buffer
+    cursor_pos = view.layer_index
 
     vao = _get_vao()
     draw_program = _get_program()
+    empty_texture = _get_empty_texture(size)
     
-    with vao, ob, draw_program:
-        gl.glViewport(0, 0, w, h)
+    with vao, offscreen_buffer:
 
         gl.glEnable(gl.GL_BLEND)
         gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
         gl.glClearBufferfv(gl.GL_COLOR, 0, EMPTY_COLOR)
+        gl.glViewport(0, 0, w, h)
 
-        cursor_pos = view.layer_index
-        overlay = view.overlay
-        overlay_texture = _get_overlay_texture(size)
+        with draw_program, drawing_texture:
 
-        if overlay.dirty and overlay.lock.acquire(timeout=0.01):
-            rect = overlay.dirty
-            x0, y0, x1, y1 = rect.box()
-            overlay_data = overlay.data[x0:x1, y0:y1].tobytes("F")
-            gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 4)
-            gl.glTextureSubImage2D(overlay_texture.name, 0, *rect.position, *rect.size,
-                                   gl.GL_RGBA_INTEGER, gl.GL_UNSIGNED_BYTE, overlay_data)
-            overlay.dirty = None
-            overlay.lock.release()
+            gl.glUniform4fv(4, 256, colors)
 
-        # Set current palette
-        gl.glUniform4fv(2, 256, colors)
-
-        gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)  # Needed for reading 8bit data
-
-        for i in range(d):
-            # TODO This is pretty slow, since we draw every layer every frame.
-            # A better version would be to keep two extra fbs, one for the layers
-            # below and one above, and render all non-current layers to those.
-            # As long as no other layers are modified the fbs can be re-used.
-
-            if not view.layer_visible(i):
-                continue
-
-            tex = _get_layer_texture(i, size)
-            dirty = view.dirty[i]
-            if dirty and drawing.lock.acquire(timeout=0.01):
-                layer = data[:, :, i]
-                layer_data = layer.tobytes("F")  # TODO maybe there's a better way?
-                gl.glTextureSubImage2D(tex.name, 0, 0, 0, w, h,  # TODO use dirty rect
-                                       gl.GL_RED_INTEGER, gl.GL_UNSIGNED_BYTE,
-                                       layer_data)
-                view.dirty[i] = None
-            with tex:
-                if i == cursor_pos:
-                    second_texture = overlay_texture
-                else:
-                    second_texture = _get_empty_texture(size)
-                with second_texture:
+            # Draw the layers below the current one
+            if cursor_pos > 0:
+                with empty_texture:
                     gl.glUniform1f(1, 1)
+                    gl.glUniform2i(2, 0, cursor_pos)
                     gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6)
 
-        gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 4)
+            # Draw the current layer + overlay
+            with overlay_texture:
+                gl.glUniform1f(1, 1)
+                gl.glUniform2i(2, cursor_pos, cursor_pos + 1)
+                gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6)
+                
+            # Draw the layers on top
+            if cursor_pos < d - 1:
+                with empty_texture:
+                    gl.glUniform1f(1, 1)
+                    gl.glUniform2i(2, cursor_pos + 1, d)
+                    gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6)
 
-    return ob
+    return offscreen_buffer
 
+
+# Helper functions to cache frequently used stuff.
 
 @lru_cache(1)
 def _get_vao():
@@ -103,9 +117,10 @@ def _get_offscreen_buffer(size):
     return FrameBuffer(size, textures=dict(color=Texture(size, unit=0)))
 
 
-@lru_cache(128)
-def _get_layer_texture(i, size):
-    texture = ByteTexture(size=size)
+@lru_cache(1)
+def _get_3d_texture(size):
+    "We'll store the entire image in a texture array."
+    texture = ByteTexture3D(size=size)
     texture.clear()
     return texture
 
